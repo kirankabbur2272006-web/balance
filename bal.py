b80fe6c00ce4d7d47e7dba097c18109170acded3
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2025-11-27 15:41:50 +0530

    Add balance calculation script with command line args

---
+++ bal.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) >= 3:
+  script_name = sys.argv[0]
+  intial = sys.argv[1]
+  deposit = sys.argv[1]
+else:
+  script_name = sys.argv[0]
+  intial  = 15000
+  deposit = 2000
+
+balance=intial+deposit
+print("balance:",balance)
